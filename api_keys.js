3ddb2185dfebab39d7b6f4c4b088be7ca6d125e5
sonnythedev <44914601+sonnythedev@users.noreply.github.com>
2019-03-12 16:08:02 -0400

    Update api_keys.js

--- api_keys.js
+++ api_keys.js
@@ -3,5 +3,5 @@ const apiKey='XXXXX'; ////USE YOUR OWN KEY!
 
 
 const actorUrl='http://api.themoviedb.org/3/search/person?';
-const actorApiKey='XXXXXXX; ////USE YOUR OWN KEY!
+const actorApiKey='XXXXXXX'; ////USE YOUR OWN KEY!
 const actorProfilePath='https://image.tmdb.org/t/p/w500/';
